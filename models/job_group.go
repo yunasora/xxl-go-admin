6572cb250f220af046e50abd9cb0832f6dd24242
Ubuntu <ubuntu@localhost.localdomain>
2026-06-14 22:15:36 +0800

    fix appName json

--- models/job_group.go
+++ models/job_group.go
@@ -2,7 +2,7 @@ package models
 
 type JobGroup struct {
 	ID      int64  `gorm:"primaryKey;autoIncrement" json:"id"`
-	AppName string `gorm:"type:varchar(255);not null;uniqueIndex;not null" json:appName`
+	AppName string `gorm:"type:varchar(255);not null;uniqueIndex;not null" json:"appName"`
 	Title   string `gorm:"type:varchar(255);not null" json:"title"`
 }
 
